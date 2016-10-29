d3444ea8cab320191219efedd0f9467b17670808
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-10-30 05:40:14 +0700

    route delete

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var memo = require('../models/memo')
 
-/* GET users listing. */
+/* GET All memo */
 router.get('/', function(req, res, next) {
-    res.send('this is api');
+    memo.find({}, (err, data) => {
+        res.json(data)
+    })
 });
+
+// Post new memo
+router.post('/', function(req, res, next) {
+    memo.create({
+        title: req.body.title,
+        content: req.body.content,
+        tags: req.body.tags
+    }, (err, data) => {
+        res.json(data)
+    })
+});
+
+//edit memo
+router.put('/:title', function(req, res, next) {
+    memo.update({
+        title: req.params.title
+    }, {
+        title: req.body.title,
+        content: req.body.content,
+        tags: req.body.tags
+    }, (err) => {
+        if (err) {
+            return handleError(err)
+        } else {
+            res.json({message: 'data updated'})
+        }
+    })
+})
+
+//delete memo
+router.delete('/:title', function(req, res, next) {
+    memo.remove({
+        title: req.params.title
+    }, (err) => {
+        if (err) {
+            return handleError(err)
+        } else {
+            res.json({message: 'data successfully deleted'})
+        }
+    })
+})
 
 module.exports = router;
