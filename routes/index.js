1e7682d2dd7cf38b27fa3d2c4722f57a3e18f05f
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-10-31 22:45:42 +0700

    index MVC

--- routes/index.js
+++ routes/index.js
@@ -1,70 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var memo = require('../models/memo')
+var controller = require('../controller/controller')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', {title: 'Cool Memo'});
-});
-
-router.post('/post', function(req, res, next) {
-    memo.create({
-        title: req.body.title,
-        content: req.body.content,
-        tags: req.body.tags
-    }, (err, data) => {
-        if (err) {
-            return handleError(err);
-        } else {
-            res.redirect('/list')
-        }
-    })
-})
-
-router.get('/list', function(req, res, next) {
-    memo.find({}, (err, data) => {
-        res.render('list', {data, title: "List Memo"})
-    })
-})
-
-router.get('/update/:id', function(req, res, next) {
-    memo.findOne({
-        _id: req.params.id
-    }, (err, data) => {
-        res.render('update', {data})
-    })
-})
-
-router.post('/update/:id', function(req, res, next) {
-    memo.update({
-        _id: req.params.id
-    }, {
-        title: req.body.title,
-        content: req.body.content,
-        tags: req.body.tags
-    }, (err) => {
-        if (err) {
-            return handleError(err)
-        } else {
-            res.redirect('/list')
-        }
-    })
-})
-
-router.get('/delete/:id', function(req, res, next) {
-    memo.remove({
-        _id: req.params.id
-    }, (err) => {
-        if (err) {
-            return handleError(err)
-        } else {
-            res.redirect('/list')
-        }
-    })
-})
-
-router.get('/ajax', function(req, res, next) {
-    res.render('ajax', {title: 'AJAX TABLE'})
-})
+router.get('/', controller.getHome);
+router.post('/post', controller.createMemo)
+router.get('/list', controller.showAllMemo)
+router.get('/update/:id', controller.updateMemo)
+router.post('/update/:id', controller.postUpdateMemo)
+router.get('/delete/:id', controller.deleteMemo)
+router.get('/ajax', controller.getAjaxPage)
 
 module.exports = router;
